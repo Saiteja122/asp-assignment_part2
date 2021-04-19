1df56a30cb5aae4c7d10fa826452085ff4d6ae47
Your Name <you@example.com>
2021-04-19 03:10:15 +0300

    new file:   Migrations/20210419000640_InitialCreate.cs 	new file:   Migrations/TodoDbContextModelSnapshot.cs 	modified:   SportingPower.db 	modified:   Views/Home/Index.cshtml 	modified:   Views/Shared/_Layout.cshtml 	modified:   bin/Debug/net5.0/ref/todoApp.dll 	modified:   bin/Debug/net5.0/todoApp.Views.dll

---
+++ Migrations/20210419000640_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace todoApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Todo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    AddedDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DoneDate = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Done = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    UserMail = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Todo", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Todo");
+        }
+    }
+}
