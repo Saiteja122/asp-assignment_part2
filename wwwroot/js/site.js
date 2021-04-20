0cdb53735ce9c2d02bd01a83c964a5dd0899dd27
Your Name <you@example.com>
2021-04-20 16:21:49 +0300

    modified:   Views/Home/Index.cshtml
    	modified:   Views/Shared/_Layout.cshtml
    
    	modified:   Views/Todo/Create.cshtml
    	modified:   Views/Todo/Edit.cshtml
    	modified:   Views/Todo/Index.cshtml
    	modified:   wwwroot/js/site.js
    	new file:   wwwroot/manifest.json
    	new file:   wwwroot/serviceworker.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function copy_to_clipboard() {
+  var copyText = document.getElementsByClassName('title')[0].innerHTML;
+  
+
+  copyText = copyText.replace(/\s/g, '');
+  // alert("Copied the title : " + copyText);
+  
+  // copyText.select()
+
+    var dummy = document.createElement("textarea");
+    document.body.appendChild(dummy);
+
+    dummy.value = copyText;
+    dummy.select();
+
+  document.execCommand("copy");
+document.body.removeChild(dummy);
+  alert("Copied the title: " + copyText);
+
+  console.log(copyText);
+} 
